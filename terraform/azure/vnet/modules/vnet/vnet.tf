e6a801516ef67066bde8b400090f2513d1ba29bb
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2023-01-05 11:52:43 +0100

    Fix: set use_for_each variable
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/azure/vnet/modules/vnet/vnet.tf
+++ terraform/azure/vnet/modules/vnet/vnet.tf
@@ -18,6 +18,8 @@ module "vnet" {
   source  = "Azure/vnet/azurerm"
   version = "4.0.0"
 
+  use_for_each = true
+
   vnet_name           = var.vnet_name
   vnet_location       = var.resource_group_location
   resource_group_name = azurerm_resource_group.vnet.name
