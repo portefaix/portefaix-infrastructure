e4c28f561ba4f36989016401d1b0c9a402b6d1ab
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2023-04-17 17:17:42 +0200

    Update: CIS AWS Foundations v1.4.0
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/aws/security-hub/modules/security-hub/security_hub.tf
+++ terraform/aws/security-hub/modules/security-hub/security_hub.tf
@@ -55,7 +55,7 @@ resource "aws_securityhub_standards_subscription" "aws_foundational" {
 resource "aws_securityhub_standards_subscription" "cis" {
   provider      = aws.audit
   count         = var.enable_cis ? 1 : 0
-  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.4.0"
 
   depends_on = [
     aws_securityhub_account.this,
