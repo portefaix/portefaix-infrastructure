b2b2384d8f0052092d20049de9f216d80d097cd0
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-08-27 15:09:43 +0200

    feat(talos): tunnel
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/talos/zero-trust/modules/zero-trust/access.tf
+++ terraform/talos/zero-trust/modules/zero-trust/access.tf
@@ -18,10 +18,11 @@ resource "random_id" "tunnel_secret" {
   byte_length = 35
 }
 
-resource "cloudflare_tunnel" "this" {
-  account_id = var.cloudflare_account_id
-  name       = var.tunnel_name
-  secret     = random_id.tunnel_secret.b64_std
+resource "cloudflare_zero_trust_tunnel_cloudflared" "this" {
+  account_id    = var.cloudflare_account_id
+  name          = var.tunnel_name
+  config_src    = "cloudflare"
+  tunnel_secret = random_id.tunnel_secret.b64_std
 }
 
 # resource "cloudflare_record" "this" {
