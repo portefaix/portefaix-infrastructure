9e25bb37eaf039c8d806dfb08a8385741df8b07f
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-08-29 09:17:15 +0200

    feat(aws): typo
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/aws/spacelift/backend.tf
+++ terraform/aws/spacelift/backend.tf
@@ -16,16 +16,5 @@
 
 terraform {
   backend "s3" {
-    region = "us-east-1" # "auto"
-
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_metadata_api_check     = true
-    skip_s3_checksum            = true
-
-    endpoints {
-      s3 = format("https://%s.r2.cloudflarestorage.com", var.cloudflare_account_id)
-    }
   }
 }
