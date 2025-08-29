1fff48575b2a8c50d164004f3457dd00bf4e8e4f
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-08-29 08:49:54 +0200

    feat(aws): split Fargate
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ terraform/aws/eks/modules/eks/fargate.tf
@@ -0,0 +1,16 @@
+module "fargate_profile" {
+  source  = "terraform-aws-modules/eks/aws//modules/fargate-profile"
+  version = "21.1.5"
+
+  cluster_name = var.cluster_name
+  name         = var.fargate_profile_name
+
+  selectors = var.fargate_selectors
+
+  tags = merge(
+    { "Name" = var.fargate_profile_name },
+    var.cluster_tags,
+    var.fargate_tags,
+    var.tags
+  )
+}
