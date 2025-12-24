fb267a2d9c4db6d3a6ec2345558d9aa031127b99
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-12-24 10:58:00 +0100

    feat(oci): use environment variables
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/oci/hub/shared/variables.tf
+++ terraform/oci/hub/shared/variables.tf
@@ -1,30 +1,30 @@
 #############################################################################
 # Provider
 
-variable "tenancy_ocid" {
-  description = "tenancy id where to create the sources"
-  type        = string
-}
+# variable "tenancy_ocid" {
+#   description = "tenancy id where to create the sources"
+#   type        = string
+# }
 
-variable "user_ocid" {
-  description = "id of user that terraform will use to create the resources"
-  type        = string
-}
+# variable "user_ocid" {
+#   description = "id of user that terraform will use to create the resources"
+#   type        = string
+# }
 
-variable "fingerprint" {
-  description = "fingerprint of oci api private key"
-  type        = string
-}
+# variable "fingerprint" {
+#   description = "fingerprint of oci api private key"
+#   type        = string
+# }
 
 variable "oci_private_key" {
   description = "OCI api private key used"
   type        = string
 }
 
-variable "region" {
-  description = "the oci region where resources will be created"
-  type        = string
-}
+# variable "region" {
+#   description = "the oci region where resources will be created"
+#   type        = string
+# }
 
 #############################################################################
 # Hub
