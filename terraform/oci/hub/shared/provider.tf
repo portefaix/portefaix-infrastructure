ac97b8e70dfb5fa7eb0c13e8e9cc33bc4ce60659
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-12-24 10:56:58 +0100

    feat(oci): clean variables
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/oci/hub/shared/provider.tf
+++ terraform/oci/hub/shared/provider.tf
@@ -1,8 +1,7 @@
 provider "oci" {
-  region       = var.region
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  # private_key  = var.private_key
+  # region       = var.region
+  # tenancy_ocid = var.tenancy_ocid
+  # user_ocid    = var.user_ocid
+  # fingerprint  = var.fingerprint
   private_key = base64decode(var.oci_private_key)
 }
